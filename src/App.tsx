104611c1dfe252dcb6ad527ff34b395b7b784f68
mahmoodyasir <yasirmamun16@gmail.com>
2024-09-28 16:27:25 +0600

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <Typography className='text-4xl'>Test</Typography>
+    </div>
+  )
+}
+
+export default App
